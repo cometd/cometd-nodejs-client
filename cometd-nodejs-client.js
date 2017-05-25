a8853f675412c4e2309e28cdf1b642b95115b074
Simone Bordet <simone.bordet@gmail.com>
2017-05-25 14:11:50 +0200

    Fixes #6 - Support HTTPS.

--- cometd-nodejs-client.js
+++ cometd-nodejs-client.js
@@ -1,7 +1,8 @@
 module.exports = {
     adapt: function() {
         var url = require('url');
-        var http = require('http');
+        var httpc = require('http');
+        var https = require('https');
 
         global.window = {};
 
@@ -12,10 +13,17 @@ module.exports = {
         window.console.debug = window.console.log;
 
         // Fields shared by all XMLHttpRequest instances.
-        var _agent = new http.Agent({
+        var _agentc = new httpc.Agent({
+            keepAlive: true
+        });
+        var _agents = new https.Agent({
             keepAlive: true
         });
         var _cookieStore = {};
+
+        function _secure(uri) {
+            return /^https/i.test(uri.protocol);
+        }
 
         // Bare minimum XMLHttpRequest implementation that works with CometD.
         window.XMLHttpRequest = function() {
@@ -30,7 +38,7 @@ module.exports = {
             this.open = function(method, uri) {
                 _config = url.parse(uri);
                 _config.method = method;
-                _config.agent = _agent;
+                _config.agent = _secure(_config) ? _agents : _agentc;
                 _config.headers = {};
                 this.readyState = window.XMLHttpRequest.OPENED;
             };
@@ -59,6 +67,7 @@ module.exports = {
                 }
 
                 var self = this;
+                var http = _secure(_config) ? https : httpc;
                 _request = http.request(_config, function(response) {
                     var success = false;
                     self.status = response.statusCode;
@@ -105,10 +114,10 @@ module.exports = {
                     });
                 });
                 ['abort', 'aborted', 'error'].forEach(function(event) {
-                    _request.on(event, function() {
+                    _request.on(event, function(x) {
                         self.readyState = window.XMLHttpRequest.DONE;
                         if (self.onerror) {
-                            self.onerror();
+                            self.onerror(x);
                         }
                     });
                 });
@@ -122,6 +131,10 @@ module.exports = {
                 if (_request) {
                     _request.abort();
                 }
+            };
+
+            this._config = function() {
+                return _config;
             };
         };
         window.XMLHttpRequest.UNSENT = 0;
---
+++ test/https.js
@@ -0,0 +1,42 @@
+var assert = require('assert');
+var cometd = require('..');
+var https = require('https');
+var fs = require('fs');
+
+describe('https', function() {
+    var _server;
+
+    beforeEach(function() {
+        cometd.adapt();
+    });
+
+    afterEach(function() {
+        if (_server) {
+            _server.close();
+        }
+    });
+
+    it('supports https', function(done) {
+        var options = {
+            key: fs.readFileSync('test/tls/private.pem'),
+            cert: fs.readFileSync('test/tls/public.pem')
+        };
+        _server = https.createServer(options, function(request, response) {
+            response.end();
+        });
+        _server.listen(0, 'localhost', function() {
+            var port = _server.address().port;
+            console.log('listening on localhost:' + port);
+            var uri = 'https://localhost:' + port;
+            var xhr = new window.XMLHttpRequest();
+            xhr.open('GET', uri + '/');
+            // Allow self-signed certificates.
+            xhr._config().rejectUnauthorized = false;
+            xhr.onload = function() {
+                assert.strictEqual(xhr.status, 200);
+                done();
+            };
+            xhr.send();
+        });
+    });
+});
